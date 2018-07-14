a9a1a533608161c7099735621085ffdbf25610f9
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-14 18:23:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,6 +27,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.names, COUNT(book_id) from characters
+  "SELECT characters.name, COUNT(book_id) from characters
   INNER JOIN character_books ON character_books.character_id = characters.id;"
 end
